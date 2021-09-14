e2045e48272bd12215022676333f958f3dece95f
lowkey <saltyndea@gmail.com>
2021-09-14 17:25:17 -0600

    Added animated burgs to all pages

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const bar1 = document.querySelector('.bar1');
+const bar2 = document.querySelector('.bar2');
+const bar3 = document.querySelector('.bar3');
+
+const toggleBurg = function() {
+
+hamburger.classList.toggle('active');
+
+}
+
+hamburger.addEventListener('click', toggleBurg);
