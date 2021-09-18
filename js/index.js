f25d8f662213f0ba54907ee816347f90f56553d6
lowkey <saltyndea@gmail.com>
2021-09-17 20:40:51 -0600

    Fixin stuf/plannin stuff

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const bar1 = document.querySelector('.bar1');
 const bar2 = document.querySelector('.bar2');
 const bar3 = document.querySelector('.bar3');
 const overlay = document.querySelector('.overlay');
-const commentForm = document.querySelector('.comment-form')
+const commentForm = document.querySelector('.comment-form');
 
 const toggleMobMenu = function() {
   overlay.classList.toggle('active');
